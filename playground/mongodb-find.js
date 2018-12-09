423598e7f7a71dd047d7cde49057bba2591e1b21
gaspergulotta <gasper.gulotta@gmail.com>
2018-12-09 01:30:55 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c0cb151a2b627c3990cf191')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').countDocuments().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Gasper Gulotta'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    });
+    
+
+    //client.close();
+});
