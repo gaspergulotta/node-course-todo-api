9da4d6fe8683049e2213b1c84fcf1de3bba30779
gaspergulotta <gasper.gulotta@gmail.com>
2018-12-18 18:25:52 -0500

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -52,21 +52,42 @@ return user.save().then(() => {
 };
 
 UserSchema.statics.findByToken = function (token) {
-var User = this;
-var decoded;
+    var User = this;
+    var decoded;
 
-try {
-    decoded = jwt.verify(token, 'abc123');
-} catch (e) {
-    return Promise.reject();
-}
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
 
-return User.findOne({
-    '_id': decoded._id,
-    'tokens.token': token,
-    'tokens.access': 'auth'
-});
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 
 UserSchema.pre('save', function (next) {
     var user = this;
